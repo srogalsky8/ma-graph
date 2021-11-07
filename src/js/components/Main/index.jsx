68394dfdca09b4c51d241d7befa31b4c4b5d238f
Steven Rogalsky <srogalsky8@gmail.com>
2021-11-07 02:15:07 -0800

    Initial commit

---
+++ src/js/components/Main/index.jsx
@@ -0,0 +1,14 @@
+import BarChart from 'js/components/BarChart';
+import ScatterPlot from 'js/components/ScatterPlot';
+
+const Main = () => {
+  return (
+    <>
+      <div>Hello world</div>
+      <BarChart />
+      <ScatterPlot />
+    </>
+  );
+}
+
+export default Main;
